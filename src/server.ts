ab9a13ba0504ce952c0870a103623312aed74c02
Roberto Tupinamba <robertotupinamba@gmail.com>
2021-02-06 23:59:01 -0300

    Configurando o debug do VScode

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Ola Mundo!!" });
-});
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
     console.log("✨ Server started on port 3333");
